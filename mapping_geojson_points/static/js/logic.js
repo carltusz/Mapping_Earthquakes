bfe72d69ebf93dca8cab3ac3dd0e4dd754a50182
Carl Tusz <carltusz@gmail.com>
2022-02-20 19:13:48 -0500

    completed multiple layers

--- mapping_geojson_points/static/js/logic.js
+++ mapping_geojson_points/static/js/logic.js
@@ -2,27 +2,46 @@
 console.log("working");
 
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+//let map = L.map('mapid').setView([30, 30], 2);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/carltusz/Mapping_Earthquakes/main/majorAirports.json";
+
+//console.log(d3.json(airportData));
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2><hr><h2>Airport Name: " + feature.properties.name + "</h2>");
+     }
+  }).addTo(map);
+});
+
+//console.log(airportData);
 
 // Grabbing our GeoJSON data.
 //L.geoJSON(sanFranAirport).addTo(map);
@@ -37,12 +56,21 @@ let map = L.map('mapid').setView([37.5, -122.5], 10);
 //   }
 // }).addTo(map);
 
-L.geoJSON(sanFranAirport, {
-  onEachFeature: function(feature, layer) {
-    console.log(feature);
-    layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2><hr><h2>Airport Name: " + feature.properties.name + "</h2>");
-   }
-}).addTo(map);
+
+//  L.geoJSON(sanFranAirport, {
+//    onEachFeature: function(feature, layer) {
+//      console.log(feature);
+//      layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2><hr><h2>Airport Name: " + feature.properties.name + "</h2>");
+//     }
+//  }).addTo(map);
+
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -53,5 +81,24 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/
     zoomOffset: -1,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
